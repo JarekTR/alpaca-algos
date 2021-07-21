77a583853ff4ce04794bd6040a3f5a3cd99a771a
JarekTC <carlson.jarek@gmail.com>
2021-07-21 12:58:51 -0600

    Update test.py for test account and order API calls

--- test.py
+++ test.py
@@ -1,5 +1,26 @@
-import requests
+import requests, json
 from config import *
 
-r = requests.get(ACCOUNT_URL, headers=HEADERS)
-print(r.content)
+
+def get_account():
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
+    return json.loads(r.content)
+
+
+def create_order(symbol, qty, side, type, time_in_force):
+    data = {
+        "symbol": symbol,
+        "qty": qty,
+        "side": side,
+        "type": type,
+        "time_in_force": time_in_force,
+    }
+    r = requests.post(ORDER_URL, json=data, headers=HEADERS)
+    return json.loads(r.content)
+
+
+test_account = get_account()
+test_order = create_order("AAPL", 5, "buy", "market", "gtc")
+
+print(test_account, "\n")
+print(test_order, "\n")
