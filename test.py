8d7c0453975d051dbc3b4ece80de345538e798d0
JarekTC <carlson.jarek@gmail.com>
2021-07-23 10:01:14 -0600

    Update test.py to use alpaca-trade-api instead of requests/json

--- test.py
+++ test.py
@@ -1,29 +1,65 @@
-import json
-import requests
+import alpaca_trade_api as trade_api
 from config import *
 
 
 def get_account():
-    r = requests.get(ACCOUNT_URL, headers=HEADERS)
-    return json.loads(r.content)
+    api = trade_api.REST(API_KEY, SECRET_KEY, APCA_API_BASE_URL)
+    account = api.get_account()
+    return account
 
 
-def create_order(symbol, qty, side, order_type, time_in_force):
-    data = {
-        "symbol": symbol,
-        "qty": qty,
-        "side": side,
-        "type": order_type,
-        "time_in_force": time_in_force,
-    }
-    r = requests.post(ORDER_URL, json=data, headers=HEADERS)
-    return json.loads(r.content)
+def get_quotes():
+    api = trade_api.REST(API_KEY, SECRET_KEY, APCA_API_DATA_URL)
+    return api.get_quotes("AAPL", "2021-02-08", "2021-02-08", limit=10).df
+
+
+def buy_order():
+    api = trade_api.REST(API_KEY, SECRET_KEY, APCA_API_BASE_URL)
+    order = api.submit_order(
+        "AAPL",
+        qty=5,
+        side="buy",
+        type="market",
+        time_in_force="day",
+        limit_price=None,
+        stop_price=None,
+        client_order_id=None,
+        order_class=None,
+        take_profit=None,
+        stop_loss=None,
+        trail_price=None,
+        trail_percent=None,
+        notional=None,
+    )
+    return order
+
+def sell_order():
+    api = trade_api.REST(API_KEY, SECRET_KEY, APCA_API_BASE_URL)
+    order = api.submit_order(
+        "AAPL",
+        qty=5,
+        side="sell",
+        type="market",
+        time_in_force="day",
+        limit_price=None,
+        stop_price=None,
+        client_order_id=None,
+        order_class=None,
+        take_profit=None,
+        stop_loss=None,
+        trail_price=None,
+        trail_percent=None,
+        notional=None,
+    )
+    return order
 
 
 test_account = get_account()
-test_buy = create_order("AAPL", 5, "buy", "market", "gtc")
-test_sell = create_order("AAPL", 5, "sell", "market", "gtc")
+test_quotes = get_quotes()
+test_buy = buy_order()
+test_sell = sell_order()
 
 print(test_account, "\n")
+print(test_quotes, "\n")
 print(test_buy, "\n")
 print(test_sell)
