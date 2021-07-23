31c869a9be065ff0203c346cba23b3b1cc47094f
JarekTC <carlson.jarek@gmail.com>
2021-07-23 10:30:06 -0600

    Test API calls with test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import alpaca_trade_api as trade_api
+import alpaca_trade_api
 from config import *
 
 
-def get_account(api):
-    account = api.get_account()
+def get_account(trade_api):
+    account = trade_api.get_account()
     return account
 
 
@@ -15,8 +15,8 @@ def get_trades(data_api):
     return data_api.get_trades("AAPL", "2021-02-08", "2021-02-08", limit=10).df
 
 
-def buy_order(api):
-    order = api.submit_order(
+def buy_order(trade_api):
+    order = trade_api.submit_order(
         "AAPL",
         qty=5,
         side="buy",
@@ -35,9 +35,8 @@ def buy_order(api):
     return order
 
 
-def sell_order(api):
-    api = trade_api.REST(API_KEY, SECRET_KEY, APCA_API_BASE_URL)
-    order = api.submit_order(
+def sell_order(trade_api):
+    order = trade_api.submit_order(
         "AAPL",
         qty=5,
         side="sell",
@@ -56,15 +55,16 @@ def sell_order(api):
     return order
 
 
-api = trade_api.REST(API_KEY, SECRET_KEY, APCA_API_BASE_URL)
-data_api = trade_api.REST(API_KEY, SECRET_KEY, APCA_API_DATA_URL)
+trade_api = alpaca_trade_api.REST(API_KEY, SECRET_KEY, APCA_API_BASE_URL)
+data_api = alpaca_trade_api.REST(API_KEY, SECRET_KEY, APCA_API_DATA_URL)
 
 
-test_account = get_account(api)
+test_buy = buy_order(trade_api)
+test_account = get_account(trade_api)
 test_quotes = get_quotes(data_api)
 test_trades = get_trades(data_api)
-test_buy = buy_order(api)
-test_sell = sell_order(api)
+test_sell = sell_order(trade_api)
+
 
 print(test_account, "\n")
 print(test_quotes, "\n")
